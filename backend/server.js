89f51d80a822640266385e3c9c270079ad1bb2e3
7ozaifa <huzaifasalah9@gmail.com>
2024-10-30 02:41:29 +0400

    update cors

--- backend/server.js
+++ backend/server.js
@@ -41,8 +41,6 @@ app.use(
         callback(new Error("Not allowed by CORS"));
       }
     },
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
