34be5f7f86e13049c2c4320f34d3b90d7cf6e856
7ozaifa <huzaifasalah9@gmail.com>
2024-10-30 04:00:30 +0400

    update cors

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,7 @@ const __dirname = path.dirname(__filename);
 const allowedOrigins = [
   "https://blackdantella.com",
   "https://www.blackdantella.com",
+  "https://admin.blackdantella.com",
   "http://localhost:5173",
   "http://localhost:5174",
 ];
