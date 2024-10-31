8de42d45eb555bbfae6e108c63be8a5c8f2777c8
7ozaifa <huzaifasalah9@gmail.com>
2024-10-31 12:16:22 +0400

    update the cors

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,7 @@ const allowedOrigins = [
   "https://blackdantella.com",
   "https://www.blackdantella.com",
   "https://admin.blackdantella.com",
+  "https://admin.blackdantella.com/api/products/add",
   "http://localhost:5173",
   "http://localhost:5174",
   "https://black-dantella-client.onrender.com",
